83cfcdf97b8057ff7fb839b93984b91b71ec3745
Wark0711 <mickey.rahul98@gmail.com>
2024-06-29 00:22:25 +0530

    Added sidebar, navbar and homepage components

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+    {
+        label: 'Home',
+        imgUrl: '/icons/Home.svg',
+        path: '/',
+    },
+    {
+        label: 'Upcoming',
+        imgUrl: '/icons/upcoming.svg',
+        path: '/upcoming',
+    },
+    {
+        label: 'Previous',
+        imgUrl: '/icons/previous.svg',
+        path: '/previous',
+    },
+    {
+        label: 'Recordings',
+        imgUrl: '/icons/Video.svg',
+        path: '/recordings',
+    },
+    {
+        label: 'Personal Room',
+        imgUrl: '/icons/add-personal.svg',
+        path: '/personal-room',
+    },
+]
